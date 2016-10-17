7a4ca4e7b387b3c7e18d2f294e996e5d29aff78c
Mauricio Farias Jr <mauricio199836@hotmail.com>
2016-10-17 18:58:13 +0000

    Notebook Version 0.1

--- teste/main.cpp
+++ teste/main.cpp
@@ -6,56 +6,24 @@ using namespace std;
 
 int main()
 {
-    string argumento1,argumento2,argumento3,title;
-    string texto;
     ofstream myfile;
-    while(argumento1 != "exit")
+    string title,texto,textocompleto,teste;
+    while(title != "exit")
     {
-        cin >> argumento1 >> argumento2 >> argumento3;
-        if(argumento1 == "notebook")
+        cout << "Digite o titulo(ou exit para sair): ";
+        cin >> title;
+        if(title != "exit")
         {
-            if (argumento2 == "write")
+            myfile.open(title);
+            cout << "Digite return para voltar" << endl;
+            while(texto != "return")
             {
-                if(argumento3 == "title") //funcao de criar arquivo
-                {
-                    cout << "Digite return para voltar ao inicio" << endl;
-                    cout << "Informe o titulo do arquivo: ";
-                    while(title != "return")
-                    {
-                        cin >> title;
-                        myfile.open(title);
-                    }
-                }
-                else
-                {
-                    cout << "Argumentos validos:"<<endl<< "title" <<endl;
-                }
-                if(argumento3 == "content") //funcao de escrever no arquivo
-                {
-                    cout << "Digite return para voltar ao inicio" << endl;
-                    cout << "Informe o arquivo que voce deseja escrever :" << endl;
-                    cin >> title;
-                    myfile.open(title);
-                    while(texto != "return")
-                    {
-                        cin >> texto;
-                        getline(cin,texto);
-                        myfile << texto << endl;
-                    }
-                }
-                else
-                {
-                    cout << "content"<<endl;
-                }
+                textocompleto += texto + " ";
+                cin >> texto;
             }
-            else
-            {
-                cout << "Argumento valido: write"<<endl;
-            }
-        }
-        else
-        {
-            cout << "Argumento valido : notebook"<<endl;
+            cout << "Arquivo criado" << endl << title << endl << endl << textocompleto << endl << endl;
+            myfile << textocompleto;
+            myfile.close();
         }
     }
     return 0;
