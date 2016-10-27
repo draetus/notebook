e7539e4ab828f90b2dc830a8a4eb20b089a6a871
Mauricio Farias Jr <mauricio199836@hotmail.com>
2016-10-27 19:06:48 +0000

    Notebook version 0.3

--- ProjetoClion-Sem_Argumentos/functions.h
+++ ProjetoClion-Sem_Argumentos/functions.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 std::string titulo(char); //Funcao que cria o arquivo
 
 std::string txt(std::string);  //Funcao que cria o texto inserido no arquivo
--- ProjetoClion-Sem_Argumentos/main.cpp
+++ ProjetoClion-Sem_Argumentos/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "functions.h"
 
 int main(int argc, char **argv)
 {
